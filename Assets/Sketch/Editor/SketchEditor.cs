565e47c0a4d5d1d931da61667f0406daa854f511
emil <emil@16e7374d-a1f6-0310-9a7a-f1d2bd8b5927>
2010-03-19 13:41:14 +0000

    Script cleanup, added missing meshes resources

--- Assets/Sketch/Editor/SketchEditor.cs
+++ Assets/Sketch/Editor/SketchEditor.cs
@@ -2,151 +2,144 @@ using UnityEngine;
 using UnityEditor;
 using System.Collections;
 
-//namespace UnityEditor
-//{
-	[CustomEditor (typeof (Sketch))]
-	public class SketchEditor : Editor
-	{
-		ToolType m_ActiveTool = ToolType.Movement;
-	
-		public enum ToolType { Movement, Extrusion, SomethingElse };
-		
-		public static string GetUniqueAssetName (string path, string extension)
-		{
-			int id = 0;
-			
-			while (System.IO.File.Exists (path + id + extension))
-			{
-				id++;
-			}
-			
-			return path + id + extension;
-		}
-		
-		public static string GetProjectRelativePath (string path)
-		{
-			if (path.IndexOf (Application.dataPath) != 0)
-			{
-				return path;
-			}
-			
-			return "Assets" + path.Substring (Application.dataPath.Length);
-		}
-		
-		[MenuItem ("GameObject/Create Other/Sketch Cube")]
-		public static void CreateSketchCube ()
-		{
-			GameObject cube;
-			Mesh cubeMesh;
-			string cubeMeshPath;
-			
-			cubeMeshPath = GetProjectRelativePath (GetUniqueAssetName (Application.dataPath + "/Meshes/Cube", ".blend"));
-			
-			AssetDatabase.CopyAsset ("Assets/Sketch/Meshes/Cube.blend", cubeMeshPath);
-			AssetDatabase.Refresh ();
-			cubeMesh = AssetDatabase.LoadAssetAtPath (cubeMeshPath, typeof (Mesh)) as Mesh;
-			
-			if (cubeMesh == null)
-			{
-				Debug.LogError ("No mesh");
-				return;
-			}
-			
-			cube = EditorUtility.InstantiatePrefab (AssetDatabase.LoadAssetAtPath ("Assets/Sketch/Prefabs/Cube.prefab", typeof (GameObject))) as GameObject;			
-			if (cube == null)
-			{
-				Debug.LogError ("No instance");
-				return;
-			}
-			
-			cube.GetComponent <MeshFilter> ().sharedMesh = cubeMesh;
-			cube.GetComponent <Sketch> ().UpdateCollider ();
-			
-			Selection.activeObject = cube;
-		}
-	
-		void OnSceneGUI ()
-		{
-			Sketch sketch;
-		
-			sketch = target as Sketch;
-		
-			if (sketch == null)
-			{
-				return;
-			}
-		
-			//Tools.current = 4;
-		
-			if (sketch.HasSelection ())
-			{
-				Vector3 trianglePosition = sketch.TriangleCentre (sketch.Selection), editedPosition;
-			
-				editedPosition = Handles.PositionHandle (sketch.transform.position + trianglePosition, Quaternion.identity);
-				if (editedPosition != trianglePosition)
-				{
-					switch (m_ActiveTool)
-					{
-						case ToolType.Movement:
-							sketch.MoveTriangle (sketch.Selection, editedPosition - trianglePosition);
-						break;
-						case ToolType.Extrusion:
-						break;
-						case ToolType.SomethingElse:
-						break;
-					}
-				}
-			}
-		
-			if (Event.current.type == EventType.MouseDown)
-			{
-				ResolveGUISelection (sketch);
-			}
-		
-			Handles.BeginGUI (new Rect (4.0f, 20.0f, 250.0f, 20.0f));
-				GUILayout.BeginHorizontal ();
-					m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.Movement, "Movement", GUI.skin.GetStyle ("Button")) ? ToolType.Movement : m_ActiveTool;
-					m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.Extrusion, "Extrusion", GUI.skin.GetStyle ("Button")) ? ToolType.Extrusion : m_ActiveTool;
-					m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.SomethingElse, "SomethingElse", GUI.skin.GetStyle ("Button")) ? ToolType.SomethingElse : m_ActiveTool;
-				GUILayout.EndHorizontal ();
-			Handles.EndGUI ();
-		
-			//Event.current.Use ();
-		}
-	
-		static void ResolveGUISelection (Sketch sketch)
-		{
-			RaycastHit hitInfo;
-			int hitTriangle;
+[CustomEditor (typeof (Sketch))]
+public class SketchEditor : Editor
+{
+	ToolType m_ActiveTool = ToolType.Movement;
 
-			if (Physics.Raycast (Event.current.mouseRay.origin, Event.current.mouseRay.direction, out hitInfo))
+	public enum ToolType { Movement, Extrusion, SomethingElse };
+	
+	public static string GetUniqueAssetName (string path, string extension)
+	{
+		int id = 0;
+		
+		while (System.IO.File.Exists (path + id + extension))
+		{
+			id++;
+		}
+		
+		return path + id + extension;
+	}
+	
+	public static string GetProjectRelativePath (string path)
+	{
+		if (path.IndexOf (Application.dataPath) != 0)
+		{
+			return path;
+		}
+		
+		return "Assets" + path.Substring (Application.dataPath.Length);
+	}
+	
+	[MenuItem ("GameObject/Create Other/Sketch Cube")]
+	public static void CreateSketchCube ()
+	{
+		GameObject cube;
+		Mesh cubeMesh;
+		string cubeMeshPath;
+		
+		cubeMeshPath = GetProjectRelativePath (GetUniqueAssetName (Application.dataPath + "/Meshes/Cube", ".blend"));
+		
+		AssetDatabase.CopyAsset ("Assets/Sketch/Meshes/Cube.blend", cubeMeshPath);
+		AssetDatabase.Refresh ();
+		cubeMesh = AssetDatabase.LoadAssetAtPath (cubeMeshPath, typeof (Mesh)) as Mesh;
+		
+		if (cubeMesh == null)
+		{
+			Debug.LogError ("No mesh");
+			return;
+		}
+		
+		cube = EditorUtility.InstantiatePrefab (AssetDatabase.LoadAssetAtPath ("Assets/Sketch/Prefabs/Cube.prefab", typeof (GameObject))) as GameObject;			
+		if (cube == null)
+		{
+			Debug.LogError ("No instance");
+			return;
+		}
+		
+		cube.GetComponent <MeshFilter> ().sharedMesh = cubeMesh;
+		cube.GetComponent <Sketch> ().UpdateCollider ();
+		
+		Selection.activeObject = cube;
+	}
+
+	void OnSceneGUI ()
+	{
+		Sketch sketch;
+	
+		sketch = target as Sketch;
+	
+		if (sketch == null)
+		{
+			return;
+		}
+	
+		if (sketch.HasSelection ())
+		{
+			Vector3 trianglePosition = sketch.TriangleCentre (sketch.Selection), editedPosition;
+		
+			editedPosition = Handles.PositionHandle (sketch.transform.position + trianglePosition, Quaternion.identity);
+			if (editedPosition != trianglePosition)
 			{
-				if (hitInfo.transform == sketch.transform)
+				switch (m_ActiveTool)
 				{
-					Event.current.Use ();
-			
-					hitTriangle = sketch.GetNearestTriangle (hitInfo.point/* - sketch.transform.position*/);
-					sketch.Select (hitTriangle);
-				}
-				else
-				{
-					Debug.Log ("Obstructed by " + hitInfo.transform.gameObject.name);
+					case ToolType.Movement:
+						sketch.MoveTriangle (sketch.Selection, editedPosition - trianglePosition);
+					break;
+					case ToolType.Extrusion:
+					break;
+					case ToolType.SomethingElse:
+					break;
 				}
 			}
 		}
 	
-		public override void OnInspectorGUI ()
+		if (Event.current.type == EventType.MouseDown)
 		{
-			Sketch sketch;
-		
-			sketch = target as Sketch;
-		
-			if (sketch == null)
+			ResolveGUISelection (sketch);
+		}
+	
+		Handles.BeginGUI (new Rect (4.0f, 20.0f, 250.0f, 20.0f));
+			GUILayout.BeginHorizontal ();
+				m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.Movement, "Movement", GUI.skin.GetStyle ("Button")) ? ToolType.Movement : m_ActiveTool;
+				m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.Extrusion, "Extrusion", GUI.skin.GetStyle ("Button")) ? ToolType.Extrusion : m_ActiveTool;
+				m_ActiveTool = GUILayout.Toggle (m_ActiveTool == ToolType.SomethingElse, "SomethingElse", GUI.skin.GetStyle ("Button")) ? ToolType.SomethingElse : m_ActiveTool;
+			GUILayout.EndHorizontal ();
+		Handles.EndGUI ();
+	}
+
+	static void ResolveGUISelection (Sketch sketch)
+	{
+		RaycastHit hitInfo;
+		int hitTriangle;
+
+		if (Physics.Raycast (Event.current.mouseRay.origin, Event.current.mouseRay.direction, out hitInfo))
+		{
+			if (hitInfo.transform == sketch.transform)
 			{
-				return;
-			}
+				Event.current.Use ();
 		
-			DrawDefaultInspector ();
+				hitTriangle = sketch.GetNearestTriangle (hitInfo.point);
+				sketch.Select (hitTriangle);
+			}
+			else
+			{
+				Debug.Log ("Obstructed by " + hitInfo.transform.gameObject.name);
+			}
 		}
 	}
-//}
+
+	public override void OnInspectorGUI ()
+	{
+		Sketch sketch;
+	
+		sketch = target as Sketch;
+	
+		if (sketch == null)
+		{
+			return;
+		}
+	
+		DrawDefaultInspector ();
+	}
+}
--- Assets/Sketch/Sketch.cs
+++ Assets/Sketch/Sketch.cs
@@ -1,113 +1,110 @@
 using UnityEngine;
 using System.Collections;
 
-//namespace UnityEditor
-//{
-	[ExecuteInEditMode]
-	[RequireComponent (typeof (MeshFilter))]
-	[RequireComponent (typeof (Collider))]
-	public class Sketch : MonoBehaviour
+[ExecuteInEditMode]
+[RequireComponent (typeof (MeshFilter))]
+[RequireComponent (typeof (Collider))]
+public class Sketch : MonoBehaviour
+{
+	public int m_SelectedTriangle = -1;
+
+	public Mesh Mesh
 	{
-		public int m_SelectedTriangle = -1;
-
-		public Mesh Mesh
+		get
 		{
-			get
-			{
-				return GetComponent <MeshFilter> ().sharedMesh;
-			}
-		}
-		
-		public bool HasSelection ()
-		{
-			return m_SelectedTriangle != -1;
-		}
-		
-		public void ClearSelection ()
-		{
-			m_SelectedTriangle = -1;
-		}
-		
-		public void Select (int triangle)
-		{
-			m_SelectedTriangle = triangle;
-		}
-		
-		public int Selection
-		{
-			get
-			{
-				return m_SelectedTriangle;
-			}
-		}
-		
-		public void UpdateCollider ()
-		{
-			GetComponent <MeshCollider> ().sharedMesh = Mesh;
-		}
-		
-		void Awake ()
-		{
-			if (Application.isPlaying)
-			{
-				Destroy (this);
-			}
-		}
-		
-		float GetTriangleDistance (int triangle, Vector3 position)
-		{
-			return 	(Mesh.vertices [Mesh.triangles [triangle]] - position).magnitude + 
-					(Mesh.vertices [Mesh.triangles [triangle + 1]] - position).magnitude + 
-					(Mesh.vertices [Mesh.triangles [triangle + 2]] - position).magnitude;
-		}
-		
-		public Vector3 TriangleCentre (int triangle)
-		{
-			return 	(Mesh.vertices [Mesh.triangles [triangle]] +
-					Mesh.vertices [Mesh.triangles [triangle + 1]] +
-					Mesh.vertices [Mesh.triangles [triangle + 2]]) / 3.0f;
-		}
-		
-		public int GetNearestTriangle (Vector3 position)
-		{
-			float minimumDistance = Mathf.Infinity, distance;
-			int nearestTriangle = 0;
-			
-			for (int i = 0; i < Mesh.triangles.Length; i += 3)
-			{
-				distance = GetTriangleDistance (i, position);
-				if (distance < minimumDistance)
-				{
-					minimumDistance = distance;
-					nearestTriangle = i;
-				}
-			}
-			
-			return nearestTriangle;
-		}
-		
-		public void MoveTriangle (int triangle, Vector3 offset)
-		{
-			Vector3[] verts = Mesh.vertices;
-
-			verts [Mesh.triangles [triangle]] += offset;
-			verts [Mesh.triangles [triangle + 1]] += offset;
-			verts [Mesh.triangles [triangle + 2]] += offset;
-			
-			Mesh.vertices = verts;
-			Mesh.RecalculateBounds ();
-			
-			UpdateCollider ();
-		}
-		
-		void OnDrawGizmosSelected ()
-		{
-			if (HasSelection ())
-			{
-				Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 1]]);
-				Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 2]]);
-				Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 1]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 2]]);
-			}
+			return GetComponent <MeshFilter> ().sharedMesh;
 		}
 	}
-//}
+	
+	public bool HasSelection ()
+	{
+		return m_SelectedTriangle != -1;
+	}
+	
+	public void ClearSelection ()
+	{
+		m_SelectedTriangle = -1;
+	}
+	
+	public void Select (int triangle)
+	{
+		m_SelectedTriangle = triangle;
+	}
+	
+	public int Selection
+	{
+		get
+		{
+			return m_SelectedTriangle;
+		}
+	}
+	
+	public void UpdateCollider ()
+	{
+		GetComponent <MeshCollider> ().sharedMesh = Mesh;
+	}
+	
+	void Awake ()
+	{
+		if (Application.isPlaying)
+		{
+			Destroy (this);
+		}
+	}
+	
+	float GetTriangleDistance (int triangle, Vector3 position)
+	{
+		return 	(Mesh.vertices [Mesh.triangles [triangle]] - position).magnitude + 
+				(Mesh.vertices [Mesh.triangles [triangle + 1]] - position).magnitude + 
+				(Mesh.vertices [Mesh.triangles [triangle + 2]] - position).magnitude;
+	}
+	
+	public Vector3 TriangleCentre (int triangle)
+	{
+		return 	(Mesh.vertices [Mesh.triangles [triangle]] +
+				Mesh.vertices [Mesh.triangles [triangle + 1]] +
+				Mesh.vertices [Mesh.triangles [triangle + 2]]) / 3.0f;
+	}
+	
+	public int GetNearestTriangle (Vector3 position)
+	{
+		float minimumDistance = Mathf.Infinity, distance;
+		int nearestTriangle = 0;
+		
+		for (int i = 0; i < Mesh.triangles.Length; i += 3)
+		{
+			distance = GetTriangleDistance (i, position);
+			if (distance < minimumDistance)
+			{
+				minimumDistance = distance;
+				nearestTriangle = i;
+			}
+		}
+		
+		return nearestTriangle;
+	}
+	
+	public void MoveTriangle (int triangle, Vector3 offset)
+	{
+		Vector3[] verts = Mesh.vertices;
+
+		verts [Mesh.triangles [triangle]] += offset;
+		verts [Mesh.triangles [triangle + 1]] += offset;
+		verts [Mesh.triangles [triangle + 2]] += offset;
+		
+		Mesh.vertices = verts;
+		Mesh.RecalculateBounds ();
+		
+		UpdateCollider ();
+	}
+	
+	void OnDrawGizmosSelected ()
+	{
+		if (HasSelection ())
+		{
+			Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 1]]);
+			Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 2]]);
+			Gizmos.DrawLine (transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 1]], transform.position + Mesh.vertices [Mesh.triangles [m_SelectedTriangle + 2]]);
+		}
+	}
+}
